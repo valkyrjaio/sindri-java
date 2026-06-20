df0d3e5e9f034f1693dff44127da2d9be99de450
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-06-20 01:34:23 +0000

    [Release] Update SindriInfo for v26.1.0 release

--- src/main/java/io/sindri/constant/SindriInfo.java
+++ src/main/java/io/sindri/constant/SindriInfo.java
@@ -11,7 +11,7 @@ package io.sindri.constant;
 
 public final class SindriInfo {
 
-    public static final String VERSION = "26.0.2";
-    public static final String VERSION_BUILD_DATE_TIME = "June 19 2026 18:11:57 MST";
+    public static final String VERSION = "26.1.0";
+    public static final String VERSION_BUILD_DATE_TIME = "June 19 2026 18:33:03 MST";
     public static final String ICON = "    ▗▄█████▄▖\n    ▝▀█████▀▘\n        █\n        █";
 }
