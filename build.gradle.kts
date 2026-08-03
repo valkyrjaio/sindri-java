5184baef3db35238cc1711a1b5e2054b6a40f2f4
Melech Mizrachi <melechmizrachi@gmail.com>
2026-08-02 19:09:20 -0700

    [Copyright] fix: Add the header to the entry point and the root Gradle scripts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+/*
+ * This file is part of the Sindri package.
+ *
+ * Copyright (c) 2016-present Melech Mizrachi
+ *
+ * Released under the MIT License. See LICENSE.md for details.
+ */
 
 plugins {
     java
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+/*
+ * This file is part of the Sindri package.
+ *
+ * Copyright (c) 2016-present Melech Mizrachi
+ *
+ * Released under the MIT License. See LICENSE.md for details.
+ */
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
